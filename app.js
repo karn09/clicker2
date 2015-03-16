61ff2578077d2d78744437d1b6b7d9cee461cf58
John Nieves <karn09@gmail.com>
2015-03-16 01:19:54 +0000

    created admin panel to edit cat model from site

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-
 // ====================================================
 // =============== MODEL ==============================
 // ====================================================
 
 var model = {
+    adminPanel: {
+        mode: true,
+        on: false
+    },
     currentCat: null,
     cats: [{
         name: "Cat One",
@@ -33,27 +36,59 @@ var model = {
 // ====================================================
 
 var control = {
-    getCurrentCat: function () {
+    getCurrentCat: function() {
         return model.currentCat;
     },
-    setCurrentCat: function (cat) {
+    setCurrentCat: function(cat) {
         model.currentCat = cat;
     },
-    increment: function () {
+    increment: function() {
         model.currentCat.count++;
         catView.render()
     },
-    currentCount: function () {
+    currentCount: function() {
         return model.currentCat.count;
     },
     init: function() {
         model.currentCat = model.cats[0]
         catListView.init();
         catView.init();
+        if (model.adminPanel.mode) {
+            var button = document.getElementById('adminBtn')
+            button.addEventListener('click', function() {
+                control.setAdminPanel(true)
+                adminView.init();
+            })
+        }
     },
-    getCatList: function () {
+    getCatList: function() {
         return model.cats;
+    },
+    renderAdmin: function() {
+        if (model.adminPanel.on) {
+            adminView.render();
+        }
+    },
+    setAdminPanel: function(bool) {
+        model.adminPanel.on = bool;
+    },
+    saveForm: function() {
+
+    },
+    getAdminValues: function() {
+        var adminForm = document.forms[0];
+        this.catName = adminForm.elements["catName"].value;
+        this.catURL = adminForm.elements["catURL"].value;
+        this.catClick = adminForm.elements["catClick"].value;
+        return [this.catName, this.catURL, this.catClick] || null;
+    },
+    saveAdminValues: function() {
+        var currentValues = control.getAdminValues();
+        model.currentCat.name = currentValues[0];
+        model.currentCat.img = currentValues[1];
+        model.currentCat.count = currentValues[2];
     }
+
 }
 
 // ====================================================
@@ -63,17 +98,17 @@ var control = {
 var catListView = {
     init: function() {
         this.catListElem = document.getElementById('catlist');
-        
+
         this.render();
     },
-    render: function () {
+    render: function() {
         var cats = control.getCatList();
         for (var i = 0; i < cats.length; i++) {
             var buttonElem = this.catListElem.appendChild(document.createElement('button'));
             var cat = cats[i];
             buttonElem.innerHTML = cat.name;
             buttonElem.className += "btn btn-primary"
-            buttonElem.addEventListener('click', (function (cat) {
+            buttonElem.addEventListener('click', (function(cat) {
                 return function() {
                     control.setCurrentCat(cat);
                     catView.render();
@@ -84,23 +119,65 @@ var catListView = {
 };
 
 var catView = {
-    init: function () {
+    init: function() {
         this.catViewElem = document.getElementById('catView');
         this.catImgElem = document.getElementById('catImg');
         this.catCounter = document.getElementById('counter');
         this.catNameElem = document.getElementById('catName')
-        this.catImgElem.addEventListener('click', function () {
+        this.catImgElem.addEventListener('click', function() {
             control.increment()
         })
-        
+
         this.render();
     },
-    render: function () {
+    render: function() {
         var currentCat = control.getCurrentCat();
-        
+
         this.catImgElem.src = currentCat.img;
         this.catNameElem.textContent = currentCat.name;
         this.catCounter.textContent = currentCat.count;
+        control.renderAdmin();
+    }
+}
+
+var adminView = {
+    init: function() {
+        var adminPanel = document.getElementById('adminPanel')
+        adminPanel.style.display = 'block';
+        this.adminForm = document.forms[0];
+        this.catName = this.adminForm.elements["catName"];
+        this.catURL = this.adminForm.elements["catURL"];
+        this.catClick = this.adminForm.elements["catClick"];
+        var cancel = document.getElementById('cancel');
+        var save = document.getElementById('save')
+
+        cancel.addEventListener('click', (function(panel) {
+            return function() {
+                console.log('cancel clicked')
+                control.setAdminPanel(false);
+                panel.style.display = 'none';
+            }
+        })(adminPanel))
+
+        save.addEventListener('click', (function(panel) {
+            return function() {
+                control.saveAdminValues();
+                catView.render();
+                control.setAdminPanel(false);
+                panel.style.display = 'none';
+
+            }
+        })(adminPanel))
+        this.render();
+    },
+    render: function() {
+        var currentCat = control.getCurrentCat();
+
+
+        this.catName.value = currentCat.name;
+        this.catURL.value = currentCat.img;
+        this.catClick.value = currentCat.count;
+
     }
 }
 
